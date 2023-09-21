49206909d97971a50b38946c5f1fb67baf53f8a1
hygeng <genghaoyu98@sjtu.edu.cn>
2023-09-21 15:33:53 +0800

    debug regret

--- openpto/expmanager/utils_manager.py
+++ openpto/expmanager/utils_manager.py
@@ -4,6 +4,8 @@ import time
 
 import torch
 
+from openpto.metrics.regret import regret_func
+
 
 def move_to_gpu(problem, device):
     for key, value in inspect.getmembers(problem, lambda a: not (inspect.isroutine(a))):
@@ -47,52 +49,61 @@ def print_metrics(
             # Decision Quality
             preds = model(Xs)
 
-            Zs_hat, objective_hat = problem.get_decision(
+            Zs_hat, _ = problem.get_decision(
                 preds.cpu().numpy(),
                 params=Ys_aux,
                 optSolver=optSolver,
                 isTrain=isTrain,
                 **problem.init_API(),
             )
-
+            objective_hat = problem.get_objective(
+                Zs_hat, preds.cpu().numpy(), **problem.init_API()
+            )
             # Loss and Error
-            if partition != "test":
-                losses = []
-                preds = model(Xs)
-                for idx in range(len(Xs)):
-                    pred = preds[[idx]]
+            losses = []
+            preds = model(Xs)
+            for idx in range(len(Xs)):
+                pred = preds[[idx]]
 
-                    losses.append(
-                        loss_fn(
-                            problem,
-                            coeff_hat=pred,
-                            coeff_true=Ys[[idx]],
-                            params=Ys_aux[idx],
-                            partition=partition,
-                            index=idx,
-                            **model_args,
-                        )
+                losses.append(
+                    loss_fn(
+                        problem,
+                        coeff_hat=pred,
+                        coeff_true=Ys[[idx]],
+                        params=Ys_aux[idx],
+                        partition=partition,
+                        index=idx,
+                        **model_args,
                     )
+                )
 
-                losses = torch.stack(losses).flatten()
+            losses = torch.stack(losses).flatten()
+            if partition == "train":
                 test_time = 0
-            else:
-                # timing
+                regret = torch.zeros_like(losses)
+            elif partition == "val":
+                test_time = 0
+                regret = regret_func(problem, Ys, problem.z_val_opt, Zs_hat)
+            elif partition == "test":
                 test_time = time.time() - time_test_start
-                # loss
-                losses = torch.zeros_like(torch.Tensor(objective_hat))
+                regret = regret_func(problem, Ys, problem.z_test_opt, Zs_hat)
+            else:
+                raise ValueError(f"Unknown partition {partition}")
 
             # Print
             loss = losses.mean().item()
             # mae = torch.nn.L1Loss()(losses, -objectives).item()
             metrics[partition] = {
-                "objective": objective_hat,
                 "loss": loss,
                 "time": test_time,
                 "preds": preds,
+                "sols_hat": Zs_hat,
+                "objective": objective_hat,
+                "regret": regret,
             }
             logger.info(
-                f"{prefix:<6} {partition:<6} Objective: {objective_hat.mean():.6f}, {'Loss':>5}: {loss:.6f}"
+                f"{prefix:<6} {partition:<6} Objective: {objective_hat.mean():.6f}, {'Loss':>5}: {loss:.6f} "
+                f"{'Regret':>6}: {regret.mean():.6f}"
             )
         logger.info("----\n")
     return metrics
