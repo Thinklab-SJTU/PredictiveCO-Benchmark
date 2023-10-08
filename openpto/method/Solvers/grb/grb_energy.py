d1ec69921475523f0774b400bd6ca0a34b7df973
Averydreaming <3107264966@qq.com>
2023-10-08 15:26:36 +0800

    why solver?

--- openpto/method/Solvers/grb/grb_energy.py
+++ openpto/method/Solvers/grb/grb_energy.py
@@ -53,105 +53,99 @@ class ICONGrbSolver(optGrbSolver):
         self._getModel()
 
     def _getModel(self):
+        
         Machines = range(self.nbMachines)
         Tasks = range(self.nbTasks)
         Resources = range(self.nbResources)
+
         MC = self.MC
-        U = self.U
+        U =  self.U
         D = self.D
         E = self.E
         L = self.L
+        P = self.P
+        idle = self.idle
+        up = self.up
+        down = self.down
         relax = self.relax
-        q = self.q
-        N = 1440 // q
-
+        q= self.q
+        N = 1440//q
+        # create a model
         M = gp.Model("icon")
         if not self.verbose:
-            M.setParam("OutputFlag", 0)
+            M.setParam('OutputFlag', 0)
         if relax:
-            x = M.addVars(
-                Tasks, Machines, range(N), lb=0.0, ub=1.0, vtype=GRB.CONTINUOUS, name="x"
-            )
+            x = M.addVars(Tasks, Machines, range(N), lb=0., ub=1., vtype=GRB.CONTINUOUS, name="x")
         else:
-            x = M.addVars(Tasks, Machines, range(N), vtype=GRB.BINARY, name="x")
+            x = M.addVars(Tasks, Machines, range(N), vtype=GRB.BINARY, name="x") # x是0/1变量
 
-        M.addConstrs(x.sum(f, "*", range(E[f])) == 0 for f in Tasks)
-        M.addConstrs(x.sum(f, "*", range(L[f] - D[f] + 1, N)) == 0 for f in Tasks)
-        M.addConstrs(
-            (
-                gp.quicksum(x[(f, m, t)] for t in range(N) for m in Machines) == 1
-                for f in Tasks
-            )
-        )
+        M.addConstrs( x.sum(f,'*',range(E[f])) == 0 for f in Tasks) # 46c
+        M.addConstrs( x.sum(f,'*',range(L[f]-D[f]+1,N)) == 0 for f in Tasks) # 46d 
+        M.addConstrs(( gp.quicksum(x[(f,m,t)] for t in range(N) for m in Machines) == 1  for f in Tasks)) # 46b
 
         # capacity requirement
         for r in Resources:
             for m in Machines:
                 for t in range(N):
-                    M.addConstr(
-                        gp.quicksum(
-                            gp.quicksum(
-                                x[(f, m, t1)] for t1 in range(max(0, t - D[f] + 1), t + 1)
-                            )
-                            * U[f][r]
-                            for f in Tasks
-                        )
-                        <= MC[m][r]
-                    )
-
+                    M.addConstr( gp.quicksum( gp.quicksum(x[(f,m,t1)]  for t1 in range(max(0,t-D[f]+1),t+1) )*
+                                   U[f][r] for f in Tasks) <= MC[m][r])   
+        # M = M.presolve()
         M.update()
-        self._model = M
-        self.z = dict()
+        self.model = M
+
+        self.x = dict()
         for var in M.getVars():
             name = var.varName
-            if name.startswith("x["):
-                (f, m, t) = map(int, name[2:-1].split(","))
-                self.z[(f, m, t)] = var
+            if name.startswith('x['):
+                (f,m,t) = map(int, name[2:-1].split(','))
+                self.x[(f,m,t)] = var
 
     def solve(self, price, timelimit=None):
-        Model = self._model
-        D = self.D
-        P = self.P
-        q = self.q
-        N = 1440 // q
-        x = self.z
-        nbMachines = self.nbMachines
-        nbTasks = self.nbTasks
-        nbResources = self.nbResources
-        Machines = range(nbMachines)
-        Tasks = range(nbTasks)
-        range(nbResources)
-        price = move_to_array(price)
+        # Model = self._model
+        # D = self.D
+        # P = self.P
+        # q = self.q
+        # N = 1440 // q
+        # x = self.z
+        # nbMachines = self.nbMachines
+        # nbTasks = self.nbTasks
+        # nbResources = self.nbResources
+        # Machines = range(nbMachines)
+        # Tasks = range(nbTasks)
+        # range(nbResources)
+        # price = move_to_array(price)
 
-        # print( price.shape )
-        obj_expr = gp.quicksum(
-            [
-                x[(f, m, t)] * np.sum(price[t : t + D[f]]) * P[f] * q / 60
-                for f in Tasks
-                for t in range(N - D[f] + 1)
-                for m in Machines
-                if (f, m, t) in x
-            ]
-        )
+        # # print( price.shape )
+        # obj_expr = gp.quicksum(
+        #     [
+        #         x[(f, m, t)] * np.sum(price[t : t + D[f]]) * P[f] * q / 60
+        #         for f in Tasks
+        #         for t in range(N - D[f] + 1)
+        #         for m in Machines
+        #         if (f, m, t) in x
+        #     ]
+        # )
 
-        Model.setObjective(obj_expr, GRB.MAXIMIZE)
-        if timelimit:
-            Model.setParam("TimeLimit", timelimit)
-        # if relax:
-        #    Model = Model.relax()
-        Model.setParam("Method", self.method)
-        # logging.info("Number of constraints:%d", Model.NumConstrs)
+        # Model.setObjective(obj_expr, GRB.MAXIMIZE)
+        # if timelimit:
+        #     Model.setParam("TimeLimit", timelimit)
+        # # if relax:
+        # #    Model = Model.relax()
+        # Model.setParam("Method", self.method)
+        # # logging.info("Number of constraints:%d", Model.NumConstrs)
         # Model.optimize()
-        # print("###################  Value after optimization #########################")
-        # print(x)
+        # # print("###################  Value after optimization #########################")
+        # # print(x)
 
-        np.zeros(N)
-        schedule = np.zeros((nbTasks, nbMachines, N))
+        # solver=np.zeros(N)
+        # schedule = np.zeros((nbTasks, nbMachines, N))
+        # print(Model.status,GRB.Status.OPTIMAL)
         # if Model.status in [GRB.Status.OPTIMAL, 9]:
         #     try:
-        #         # task_on = Model.getAttr('x',x)
+        #         print("!!")
+        #         #task_on = Model.getAttr('x',x)
         #         task_on = np.zeros((nbTasks, nbMachines, N))
-        #         # print(x.items())
+        #         print(x.items())
         #         for (f, m, t), var in x.items():
         #             # print('im here 4')
         #             try:
@@ -171,19 +165,13 @@ class ICONGrbSolver(optGrbSolver):
         #                 for f in Tasks
         #             )
         #         solver = solver * q / 60
-        #         for f in Tasks:
-        #             for m in Machines:
-        #                 for t in range(N):
-        #                     schedule[f, m, t] = task_on[(f, m, t)]
         #         self._model.reset(0)
-        #         schedule = schedule.reshape(-1)
-        #         return schedule
+        #         return solver
         #     except NameError:
         #         print("\n__________Something wrong_______ \n ")
         #         # make sure cut is removed! (modifies model)
         #         self._model.reset(0)
-        #         schedule = schedule.reshape(-1)
-        #         return schedule
+        #         return solver
 
         # elif Model.status == GRB.Status.INF_OR_UNBD:
         #     print("Model is infeasible or unbounded")
@@ -193,6 +181,79 @@ class ICONGrbSolver(optGrbSolver):
         #     print("Model is unbounded")
         # else:
         #     print("Optimization ended with status %d" % Model.status)
-        # self.model.reset(0)
-        schedule = schedule.reshape(-1)
-        return schedule
+        # self._model.reset(0)
+        # schedule = schedule.reshape(-1)
+        # return solver
+        Model = self.model
+        MC = self.MC
+        U =  self.U
+        D = self.D
+        E = self.E
+        L = self.L
+        P = self.P
+        idle = self.idle
+        up = self.up
+        down = self.down
+        q= self.q
+        N = 1440//q  
+        x =  self.x
+        nbMachines = self.nbMachines
+        nbTasks = self.nbTasks
+        nbResources = self.nbResources
+        Machines = range(nbMachines)
+        Tasks = range(nbTasks)
+        Resources = range(nbResources)
+        
+        obj_expr = gp.quicksum( [x[(f,m,t)]*sum(price[t:t+D[f]])*P[f]*q/60 
+            for f in Tasks for t in range(N-D[f]+1) for m in Machines if (f,m,t) in x] )
+        
+        Model.setObjective(obj_expr, GRB.MINIMIZE)
+        #Model.setObjective( quicksum( (x[(f,m,t)]*P[f]*quicksum([price[t+i] for i in range(D[f])])*q/60) for f in Tasks
+        #                for m in Machines for t in range(N-D[f]+1)), GRB.MINIMIZE)
+        # if timelimit:
+        #     Model.setParam('TimeLimit', timelimit)
+        #if relax:
+        #    Model = Model.relax()
+        Model.setParam('Method', self.method)
+        #logging.info("Number of constraints%d",Model.NumConstrs)
+        Model.optimize()
+        
+        solver = np.zeros(N)
+        if Model.status in [GRB.Status.OPTIMAL]:
+            try:
+                #task_on = Model.getAttr('x',x)
+                task_on = np.zeros( (nbTasks,nbMachines,N) )
+                for ((f,m,t),var) in x.items():
+                    try:
+                        task_on[f,m,t] = var.X
+                    except AttributeError:
+                        task_on[f,m,t] = 0.
+                        print("AttributeError: b' Unable to retrieve attribute 'X'")
+                        print("__________Something WRONG___________________________")
+
+                for t in range(N):        
+                    solver[t] = sum( np.sum(task_on[f,:,max(0,t-D[f]+1):t+1])*P[f] for f in Tasks ) 
+                
+                
+                solver = solver*q/60
+                self.model.reset(0)  
+                #print(solver)
+                return solver
+            except NameError:
+                print("\n__________Something wrong_______ \n ")
+                # make sure cut is removed! (modifies model)
+                self.model.reset(0)
+                #print(solver)
+                return solver
+
+        elif Model.status == GRB.Status.INF_OR_UNBD:
+            print('Model is infeasible or unbounded')
+        elif Model.status == GRB.Status.INFEASIBLE:
+            print('Model is infeasible')
+        elif Model.status == GRB.Status.UNBOUNDED:
+            print('Model is unbounded')
+        else:
+            print('Optimization ended with status %d' % Model.status)
+        self._model.reset(0)
+
+        return solver
--- shells/energy.sh
+++ shells/energy.sh
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-EPOCHS=1
-GPU=-1
-=======
 GPU=0
 EPOCHS=100
->>>>>>> fad914c78f1d1cb81d789a9487467f8848850920
 
 # Prediction-focused learning
 python rethink_exp/main_results.py --problem=energy --opt_model mse      --solver gurobi --n_epochs ${EPOCHS} --gpu ${GPU}
@@ -25,5 +20,5 @@ python rethink_exp/main_results.py --problem=energy --opt_model blackbox --solve
 python rethink_exp/main_results.py --problem=energy --opt_model identity --solver gurobi --n_ptr_epochs 20 --n_epochs 10 --gpu ${GPU}
 
 
-python rethink_exp/main_results.py --problem=energy --opt_model pointLTR --solver gurobi --n_epochs 1 --gpu -1 --loadnew True
+python rethink_exp/main_results.py --problem=energy --opt_model mse --solver gurobi --n_epochs 1 --gpu -1 --loadnew True
 
