4a5834a629634447fe30403c03303ee195169c5e
hygeng <genghaoyu98@sjtu.edu.cn>
2023-10-29 02:25:39 +0000

    MSE device

--- openpto/method/Models/MSE.py
+++ openpto/method/Models/MSE.py
@@ -172,7 +172,7 @@ class DFL(optModel):
         )
 
         sol_hat = to_tensor(sol_hat).to(problem.device)
-        obj_hat = problem.get_objective(coeff_hat, sol_hat, params, **problem.init_API())
+        obj_hat = problem.get_objective(coeff_hat, sol_hat, params, **problem.init_API()).to(problem.device)
         # loss
         twostage_loss = twostageloss(problem, coeff_hat, coeff_true, **hyperparams)
         if self.optSolver.modelSense == GRB.MINIMIZE:
