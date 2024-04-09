5524d9099aa14fd59af8758719eae2487477872c
hygeng <genghaoyu98@sjtu.edu.cn>
2024-04-09 15:30:21 +0800

    update shells

--- shells/benchmarks/portfolio.sh
+++ shells/benchmarks/portfolio.sh
@@ -8,12 +8,13 @@ python rethink_exp/main_results.py --problem=portfolio --opt_model mse      --so
 python rethink_exp/main_results.py --problem=portfolio --opt_model dfl      --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
 python rethink_exp/main_results.py --problem=portfolio --opt_model blackbox --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
 python rethink_exp/main_results.py --problem=portfolio --opt_model identity --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=portfolio --opt_model spo      --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=portfolio --opt_model nce      --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=portfolio --opt_model pointLTR --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=portfolio --opt_model listLTR  --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=portfolio --opt_model pairLTR  --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=portfolio --opt_model lodl     --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --method_path "openpto/config/models/lodl50k.yaml"
+python rethink_exp/main_results.py --problem=portfolio --opt_model cpLayer  --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
+python rethink_exp/main_results.py --problem=portfolio --opt_model spo      --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=portfolio --opt_model nce      --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=portfolio --opt_model pointLTR --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=portfolio --opt_model listLTR  --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=portfolio --opt_model pairLTR  --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=portfolio --opt_model lodl     --solver cvxpy --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1 --method_path "openpto/config/models/lodl50k.yaml"
 
 # prediction + decision
 python rethink_exp/main_results.py --problem=portfolio --opt_model blackbox --solver cvxpy --prefix "ptr-ftn" --n_ptr_epochs 150 --n_epochs 150 --gpu ${GPU} --prefix "bench"
--- shells/benchmarks/shortestpath.sh
+++ shells/benchmarks/shortestpath.sh
@@ -9,11 +9,11 @@ python rethink_exp/main_results.py --problem=shortestpath --opt_model bce      -
 python rethink_exp/main_results.py --problem=shortestpath --opt_model dfl      --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
 python rethink_exp/main_results.py --problem=shortestpath --opt_model blackbox --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
 python rethink_exp/main_results.py --problem=shortestpath --opt_model identity --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=shortestpath --opt_model spo      --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=shortestpath --opt_model nce      --solver heuristic --pred_model ${P_MODEL} --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=shortestpath --opt_model pointLTR --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=shortestpath --opt_model listLTR  --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
-python rethink_exp/main_results.py --problem=shortestpath --opt_model pairLTR  --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench"
+python rethink_exp/main_results.py --problem=shortestpath --opt_model spo      --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=shortestpath --opt_model nce      --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=shortestpath --opt_model pointLTR --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=shortestpath --opt_model listLTR  --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
+python rethink_exp/main_results.py --problem=shortestpath --opt_model pairLTR  --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --batch_size 1
 python rethink_exp/main_results.py --problem=shortestpath --opt_model lodl     --solver heuristic --pred_model ${P_MODEL} --n_epochs ${EPOCHS} --gpu ${GPU} --prefix "bench" --method_path "openpto/config/models/lodl50k.yaml"
 
 # prediction + decision
