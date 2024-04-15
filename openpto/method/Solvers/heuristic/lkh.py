1be2f0ef60feb2dd24464025db52e1e8ee75dc5e
hygeng <genghaoyu98@sjtu.edu.cn>
2024-04-15 10:59:12 +0800

    starting tsp ood

--- openpto/method/Solvers/heuristic/lkh.py
+++ openpto/method/Solvers/heuristic/lkh.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 """
+
+https://github.com/fikisipi/elkai
+
 """
 
 import elkai
--- rethink_exp/collect_results.py
+++ rethink_exp/collect_results.py
@@ -51,6 +51,8 @@ global_data_names = [
     "budgetalloc-real",
     "cubic-gen",
     "bipartitematching-cora",
+    "shortestpath-warcraft"
+    "TSP-gen"
 ]
 global_model_names = [
     "mse",
